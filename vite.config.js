0d914c0fc69974a6a61c4dbfab871e80b2c61d81
victorhugoguimaraes <victorguinascimento@gmail.com>
2025-03-09 21:44:33 -0300

    fix: configuração do GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Iraí - Calculadora de IRA',
+        short_name: 'Iraí',
+        theme_color: '#059669',
+        icons: [
+          {
+            src: '/logo.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 5177,
     host: true
   },
-  base: '/'
+  base: '/Irai/'
 })
